1ab013cd51844db86b1069efff6503c7ba8446bb
udv-code <dv4code@gmail.com>
2020-09-21 11:31:32 +0300

    Added cube properties. Modified gitignore.

--- examples/sandbox/sandbox.cpp
+++ examples/sandbox/sandbox.cpp
@@ -19,16 +19,24 @@ protected:
 		// region Setup rendering
 		// Vertices
 		float vertices[] = {
-				// Positions           // Colors
-				0.0f, 0.5f, 0.0f, 0.5f, 0.5f, 0.0f, 1.0f,
-				0.0f, -0.5f, 0.0f, 0.5f, 0.0f, 0.5f, 1.0f,
-				0.5f, 0.0f, 0.0f, 0.0f, 0.5f, 0.5f, 1.0f,
-				-0.5f, 0.0f, 0.0f, 0.5f, 0.5f, 0.5f, 1.0f,
+			// Positions           // Colors
+			 0.5f,  0.5f,  0.5f,   1.0f, 1.0f, 1.0f, 1.0f, // FTR
+			 0.5f,  0.5f, -0.5f,   1.0f, 1.0f, 1.0f, 1.0f, // FBR
+			 0.5f, -0.5f, -0.5f,   1.0f, 1.0f, 1.0f, 1.0f, // FBL
+			 0.5f, -0.5f,  0.5f,   1.0f, 1.0f, 1.0f, 1.0f, // FTL
+			-0.5f,  0.5f,  0.5f,   1.0f, 1.0f, 1.0f, 1.0f, // RTR
+			-0.5f,  0.5f, -0.5f,   1.0f, 1.0f, 1.0f, 1.0f, // RBR
+			-0.5f, -0.5f, -0.5f,   1.0f, 1.0f, 1.0f, 1.0f, // RBL
+			-0.5f, -0.5f,  0.5f,   1.0f, 1.0f, 1.0f, 1.0f, // RTL
 		};
 
 		unsigned int positions[]{
-				0, 1, 2,
-				0, 1, 3
+				0, 1, 2, 3, // Front
+				4, 5, 6, 7, // Rear
+				2, 3, 6, 7, // Left
+				0, 1, 4, 5, // Right
+				0, 3, 4, 7, // Top
+				1, 2, 5, 6, // Bottom
 		};
 
 		// Vertex Array
@@ -38,9 +46,9 @@ protected:
 		// Vertex Buffer
 		vbo_.reset(vn::vertex_buffer::make(vertices, sizeof(vertices)));
 		vbo_->layout({
-				             {vn::shader_data_t::vec3, "_pos",},
-				             {vn::shader_data_t::vec4, "_color",},
-		             });
+		    { vn::shader_data_t::vec3, "_pos",  },
+		    { vn::shader_data_t::vec4, "_color",},
+		});
 
 		// Index Buffer
 		ibo_.reset(vn::index_buffer::make(positions, sizeof(positions) / sizeof(unsigned int)));
@@ -53,7 +61,7 @@ protected:
 	}
 
 	virtual void render() override {
-		camera_.rotation(45.0f);
+		// camera_.rotation(45.0f);
 
 		vn::renderer::start_scene(camera_);
 		vn::renderer::submit(vao_, shader_);
