a12696e12c67d85a97a109367e731c880d99317a
Dmitry Vasyliev <dv4code@gmail.com>
2020-10-27 12:00:25 +0200

    Improved coverage. Tried to fix sanitizers on windows!

--- cmake/sanitizers.cmake
+++ cmake/sanitizers.cmake
@@ -1,7 +1,12 @@
 include(CMakeParseArguments)
 
-set(SANITIZER_COMPILER_FLAGS "-g -O3 -fsanitize=address -fsanitize=thread -fsanitize=leak -fsanitize=undefined"
+set(SANITIZER_COMPILER_FLAGS ""
 		CACHE INTERNAL "")
+
+if (REAL_SANITIZER)
+	set(SANITIZER_COMPILER_FLAGS "-g -O3 -fsanitize=${REAL_SANITIZER}"
+			CACHE INTERNAL "")
+endif ()
 
 set(CMAKE_CXX_FLAGS_SANITIZER
 		${SANITIZER_COMPILER_FLAGS}
@@ -35,7 +40,7 @@ if (CMAKE_C_COMPILER_ID STREQUAL "GNU" OR CMAKE_Fortran_COMPILER_ID STREQUAL "GN
 endif ()
 
 function (append_sanitizer_compiler_flags)
-	set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${SANITIZER_COMPILER_FLAGS}" PARENT_SCOPE)
-	set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${SANITIZER_COMPILER_FLAGS}" PARENT_SCOPE)
+	set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${CMAKE_CXX_FLAGS_SANITIZER}" PARENT_SCOPE)
+	set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${CMAKE_C_FLAGS_SANITIZER}" PARENT_SCOPE)
 	message(STATUS "Appending code coverage compiler flags: ${SANITIZER_COMPILER_FLAGS}")
 endfunction ()
